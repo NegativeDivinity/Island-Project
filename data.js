683ea951fcb72ff136bed4bdcbfba541142597aa
Benjamin <67079423+NegativeDivinity@users.noreply.github.com>
2024-06-17 14:31:16 -0500

    Initial commit
    Just need to finish the sightseeing & transport pages, then add a description box under the filters on each page.

---
+++ data.js
@@ -0,0 +1,212 @@
+let Restaurants = [
+    {
+        "name": "Islands",
+        "cuisine": "American",
+        "rating": 4.5,
+        "img": "../imgs/res_one.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 1",
+        "cuisine": "Native",
+        "rating": 2,
+        "img": "../imgs/res_two.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 2",
+        "cuisine": "American",
+        "rating": 3,
+        "img": "../imgs/res_three.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 3",
+        "cuisine": "Pan-Asian",
+        "rating": 5,
+        "img": "../imgs/res_four.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 4",
+        "cuisine": "American",
+        "rating": 1,
+        "img": "../imgs/res_five.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 5",
+        "cuisine": "Native",
+        "rating": 4,
+        "img": "../imgs/res_one.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 6",
+        "cuisine": "Native",
+        "rating": 3.5,
+        "img": "../imgs/res_two.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 7",
+        "cuisine": "Pan-Asian",
+        "rating": 3,
+        "img": "../imgs/res_three.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 8",
+        "cuisine": "Native",
+        "rating": 2,
+        "img": "../imgs/res_four.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 9",
+        "cuisine": "Native",
+        "rating": 5,
+        "img": "../imgs/res_five.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    }
+]
+
+let Grocery = [
+    {
+        "name": "Some Name 1",
+        "type": "Supermarket",
+        "rating": 5,
+        "img": "../imgs/gro_one.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 2",
+        "type": "Supermarket",
+        "rating": 3.5,
+        "img": "../imgs/gro_two.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 3",
+        "type": "Grocery Store",
+        "rating": 2,
+        "img": "../imgs/gro_three.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 4",
+        "type": "Grocery Store",
+        "rating": 4.5,
+        "img": "../imgs/gro_four.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 5",
+        "type": "Convenience Store",
+        "rating": 4,
+        "img": "../imgs/gro_five.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    }
+]
+
+let Lodging = [
+    {
+        "name": "Some Name 1",
+        "type": "Hotel",
+        "rating": 5,
+        "img": "../imgs/hot_one.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 2",
+        "type": "Resort",
+        "rating": 3.5,
+        "img": "../imgs/hot_two.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 3",
+        "type": "Motel",
+        "rating": 2,
+        "img": "../imgs/hot_three.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 4",
+        "type": "Hotel",
+        "rating": 4.5,
+        "img": "../imgs/hot_four.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 5",
+        "type": "Motel",
+        "rating": 4,
+        "img": "../imgs/hot_five.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "name": "Some Name 6",
+        "type": "Motel",
+        "rating": 2.5,
+        "img": "../imgs/hot_six.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    }
+]
+
+let Entertainment = [
+    {
+        "activity": "Volcano Tour",
+        "scheduling_required": "Yes",
+        "phone_number": "888-888-8888",
+        "hours": "8am to 4pm",
+        "rating": 8.5,
+        "img": "../imgs/act_one.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "activity": "Taniti Museum Tour",
+        "scheduling_required": "No",
+        "phone_number": "888-888-8888",
+        "hours": "8am to 4pm",
+        "rating": 4,
+        "img": "../imgs/act_two.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "activity": "Fishing Charter",
+        "scheduling_required": "Yes",
+        "phone_number": "888-888-8888",
+        "hours": "8am to 4pm",
+        "rating": 6,
+        "img": "../imgs/act_three.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "activity": "Snorkling Expedition",
+        "scheduling_required": "No",
+        "phone_number": "888-888-8888",
+        "hours": "8am to 4pm",
+        "rating": 7.5,
+        "img": "../imgs/act_four.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "activity": "Zip-Lining",
+        "scheduling_required": "No",
+        "phone_number": "888-888-8888",
+        "hours": "8am to 4pm",
+        "rating": 3,
+        "img": "../imgs/act_five.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    },
+    {
+        "activity": "Microbrewery",
+        "scheduling_required": "No",
+        "phone_number": "888-888-8888",
+        "hours": "8am to 4pm",
+        "rating": 4.5,
+        "img": "../imgs/act_six.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus mauris sed dui convallis, a pretium sem mollis. Cras ut tincidunt ex. Nulla molestie erat sit amet aliquet mattis. Sed tempor libero quis dui iaculis, at interdum erat luctus. Donec eu tristique metus."
+    }
+]
